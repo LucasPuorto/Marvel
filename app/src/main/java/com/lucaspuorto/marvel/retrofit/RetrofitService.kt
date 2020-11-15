ef63715306b4b8d93b358026c6979e9bcc040b1a
Lucas Puorto <lucaspuorto@gmail.com>
2020-11-15 16:30:55 -0300

    criando service do retrofit

---
+++ app/src/main/java/com/lucaspuorto/marvel/retrofit/RetrofitService.kt
@@ -0,0 +1,33 @@
+package com.lucaspuorto.marvel.retrofit
+
+import com.lucaspuorto.marvel.utils.TIMEOUT_30
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import kotlin.reflect.KClass
+
+class RetrofitService(url: String) {
+
+    private val gsonConverter: GsonConverterFactory = GsonConverterFactory.create()
+
+    private val httpLoggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val httpClient = OkHttpClient.Builder()
+        .readTimeout(TIMEOUT_30, TimeUnit.SECONDS)
+        .connectTimeout(TIMEOUT_30, TimeUnit.SECONDS)
+        .writeTimeout(TIMEOUT_30, TimeUnit.SECONDS)
+        .addInterceptor(httpLoggingInterceptor)
+        .build()
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(url)
+        .client(httpClient)
+        .addConverterFactory(gsonConverter)
+        .build()
+
+    fun <T : Any> create(clazz: KClass<T>): T = retrofit.create(clazz.java)
+}
---
+++ app/src/main/java/com/lucaspuorto/marvel/utils/Constants.kt
@@ -0,0 +1,3 @@
+package com.lucaspuorto.marvel.utils
+
+const val TIMEOUT_30 = 30L
