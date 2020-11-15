4b52186866808c3fd1fc41ded7afe6252666fe62
Lucas Puorto <lucaspuorto@gmail.com>
2020-11-15 19:49:22 -0300

    removendo lista de comics no viewdata de heroi, sera adicionada outra chamada para os comics, com o ID que sera passado para a activity

--- app/src/main/java/com/lucaspuorto/marvel/presentation/viewdata/CharacterViewData.kt
+++ app/src/main/java/com/lucaspuorto/marvel/presentation/viewdata/CharacterViewData.kt
@@ -4,10 +4,5 @@ data class CharacterViewData(
     val characterName: String,
     val characterImageUrl: String,
     val characterDescription: String,
-    val characterComics: List<ComicsViewData>,
-)
-
-data class ComicsViewData(
-    val comicTitle: String,
-    val comicImageUrl: String
+    val characterId: Int
 )
--- app/src/main/java/com/lucaspuorto/marvel/presentation/viewmapper/CharacterConsultViewMapper.kt
+++ app/src/main/java/com/lucaspuorto/marvel/presentation/viewmapper/CharacterConsultViewMapper.kt
@@ -2,7 +2,6 @@ package com.lucaspuorto.marvel.presentation.viewmapper
 
 import com.lucaspuorto.marvel.data.response.Results
 import com.lucaspuorto.marvel.presentation.viewdata.CharacterViewData
-import com.lucaspuorto.marvel.presentation.viewdata.ComicsViewData
 
 object CharacterConsultViewMapper {
 
@@ -11,11 +10,6 @@ object CharacterConsultViewMapper {
             characterName = results[0].name,
             characterImageUrl = results[0].thumbnail.path + results[0].thumbnail.extension,
             characterDescription = results[0].description,
-            characterComics = listOf(
-                ComicsViewData(
-                    comicTitle = results[0].comics.items[0].name,
-                    comicImageUrl = results[0].comics.items[0].resourceURI
-                )
-            )
+            characterId = results[0].id
         )
 }
