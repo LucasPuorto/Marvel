e7f0fcd1792242169a4e404139665387bdfa2ccd
Lucas Puorto <lucaspuorto@gmail.com>
2022-09-21 21:08:39 -0300

    feat: Adding search view and favorite button

--- app/src/main/java/com/lucaspuorto/marvel/view/CharactersActivity.kt
+++ app/src/main/java/com/lucaspuorto/marvel/view/CharactersActivity.kt
@@ -1,7 +1,11 @@
 package com.lucaspuorto.marvel.view
 
 import android.os.Bundle
+import android.view.Menu
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import com.lucaspuorto.marvel.R
 import com.lucaspuorto.marvel.databinding.ActivityCharctersBinding
 
 class CharactersActivity : AppCompatActivity() {
@@ -20,5 +24,40 @@ class CharactersActivity : AppCompatActivity() {
 
     private fun setupRecyclerView() {
         binding.rvCharacters.adapter = adapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.character_menu, menu)
+        setupCharacterSearch(menu)
+        setupFavoritesButton(menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun setupCharacterSearch(menu: Menu?) {
+        val search = menu?.findItem(R.id.app_bar_search)
+        val searchView = search?.actionView as? SearchView
+
+        searchView?.let {
+            it.isSubmitButtonEnabled = false
+            it.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    //TODO: Filter character
+                    return true
+                }
+            })
+        }
+    }
+
+    private fun setupFavoritesButton(menu: Menu?) {
+        val favorites = menu?.findItem(R.id.app_bar_favorites_character)
+
+        favorites?.setOnMenuItemClickListener {
+            Toast.makeText(this, "favorites click", Toast.LENGTH_LONG).show()
+            return@setOnMenuItemClickListener true
+        }
     }
 }
