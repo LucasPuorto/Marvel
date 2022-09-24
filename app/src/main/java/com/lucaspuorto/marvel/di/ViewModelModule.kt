c3d4128790b21256d64acf4a7e21a40ef174f80a
Lucas Puorto <lucaspuorto@gmail.com>
2022-09-24 16:35:30 -0300

    fix: Adjusting di of viewmodel

--- app/src/main/java/com/lucaspuorto/marvel/di/ViewModelModule.kt
+++ app/src/main/java/com/lucaspuorto/marvel/di/ViewModelModule.kt
@@ -1,12 +1,13 @@
 package com.lucaspuorto.marvel.di
 
 import com.lucaspuorto.marvel.viewmodel.CharactersViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 object ViewModelModule {
 
     val viewModel = module {
 
-        single { CharactersViewModel(get()) }
+        viewModel { CharactersViewModel(get()) }
     }
 }
