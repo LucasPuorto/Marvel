33226eeb51600e50ce98fbac2c3e19d375d96024
Lucas Puorto <lucaspuorto@gmail.com>
2022-09-25 15:46:02 -0300

    feat: Renaming id and change favorite image

--- app/src/main/java/com/lucaspuorto/marvel/view/CharactersAdapter.kt
+++ app/src/main/java/com/lucaspuorto/marvel/view/CharactersAdapter.kt
@@ -9,17 +9,18 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.lucaspuorto.marvel.R
 import com.lucaspuorto.marvel.databinding.ItemCharcterBinding
-import com.lucaspuorto.marvel.model.CharacterModel
+import com.lucaspuorto.marvel.db.model.CharacterModel
 
 class CharactersAdapter(
-    private val characterClick: (CharacterModel) -> Unit
+    private val characterClick: (CharacterModel) -> Unit,
+    private val favoriteClick: (CharacterModel, Int) -> Unit,
 ) : ListAdapter<CharacterModel, CharactersAdapter.CharacterViewHolder>(CharacterDiffCallBack()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder =
         CharacterViewHolder.from(parent)
 
     override fun onBindViewHolder(holder: CharacterViewHolder, position: Int) {
-        holder.bind(getItem(position), characterClick)
+        holder.bind(getItem(position), position, characterClick, favoriteClick)
     }
 
     class CharacterViewHolder(private val binding: ItemCharcterBinding) : RecyclerView.ViewHolder(binding.root) {
@@ -32,7 +33,7 @@ class CharactersAdapter(
             }
         }
 
-        fun bind(character: CharacterModel, characterClick: (CharacterModel) -> Unit) {
+        fun bind(character: CharacterModel, position: Int, characterClick: (CharacterModel) -> Unit, favoriteClick: (CharacterModel, Int) -> Unit) {
             binding.apply {
                 tvCharacter.text = character.name
                 Glide.with(binding.root.context)
@@ -40,6 +41,13 @@ class CharactersAdapter(
                     .placeholder(R.drawable.ic_placeholder)
                     .diskCacheStrategy(DiskCacheStrategy.NONE)
                     .into(binding.ivCharacter)
+                mbtIsFavorite.apply {
+                    setOnClickListener { favoriteClick.invoke(character, position) }
+                    mbtIsFavorite.setBackgroundResource(
+                        if (character.isFavorite) R.drawable.ic_baseline_favorite
+                        else R.drawable.ic_baseline_favorite_border
+                    )
+                }
                 root.setOnClickListener { characterClick.invoke(character) }
             }
         }
