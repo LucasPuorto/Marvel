92f18ba21ec9a2c1661959d2dc1167ee2ebb9870
Lucas Puorto <lucaspuorto@gmail.com>
2022-09-27 21:44:26 -0300

    feat: Setting private key to a separated file to make key security

--- app/src/main/java/com/lucaspuorto/marvel/util/Md5.kt
+++ app/src/main/java/com/lucaspuorto/marvel/util/Md5.kt
@@ -4,7 +4,6 @@ import com.lucaspuorto.marvel.BuildConfig
 import com.lucaspuorto.marvel.util.Constants.HASH_TEXT_LENGTH
 import com.lucaspuorto.marvel.util.Constants.MD5
 import com.lucaspuorto.marvel.util.Constants.PAD_CHAR
-import com.lucaspuorto.marvel.util.Constants.PRIVATE_KEY
 import com.lucaspuorto.marvel.util.Constants.RADIX
 import com.lucaspuorto.marvel.util.Constants.SIGNUM_1
 import java.math.BigInteger
@@ -15,7 +14,7 @@ object Md5 {
     fun get(timestamp: String): String {
         try {
             val md = MessageDigest.getInstance(MD5)
-            val input = "$timestamp$PRIVATE_KEY${BuildConfig.PUBLIC_KEY}"
+            val input = "$timestamp${BuildConfig.PRIVATE_KEY}${BuildConfig.PUBLIC_KEY}"
             val messageDigest = md.digest(input.toByteArray())
             return BigInteger(SIGNUM_1, messageDigest).toString(RADIX).padStart(HASH_TEXT_LENGTH, PAD_CHAR)
         } catch (e: NoSuchAlgorithmException) {
